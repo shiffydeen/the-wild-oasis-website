0a387e8bc9bbd03cc03e52d1f200f61e595d1bf9
Sheriff Aderombi <ayaderombi@gmail.com>
2025-03-26 15:35:00 +0100

    progress

--- app/account/page.js
+++ app/account/page.js
@@ -7,9 +7,9 @@ export const metadata = {
 
 const Page = () => {
   return (
-    <h1>
-        Your account
-    </h1>
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+        Welcome, Sheriff
+      </h2>
   )
 }
 
