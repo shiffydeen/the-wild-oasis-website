78e0c673019a7b252bd401b466c38776ed50212f
Sheriff Aderombi <ayaderombi@gmail.com>
2025-03-27 22:50:55 +0100

    adding cabins

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,15 +1,19 @@
 import Counter from "@/app/_components/Counter"
 import Navigation from "../_components/Navigation"
 import CabinCard from "@/app/_components/CabinCard";
+// import { getCabins } from "../lib/data-service";
 
 export const metadata = {
   title: "Cabins",
 
 }
 
-export default function Page() {
+export default async function Page() {
   // CHANGE
-  const cabins = [];
+  // const cabins = await getCabins();
+
+  console.log("Starting....")
+  console.log(cabins)
 
   return (
     <div>
--- app/layout.js
+++ app/layout.js
@@ -27,9 +27,9 @@ const RootLayout = ({children}) => {
     <html lang='en'>
       <body className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}>
         <Header />
-        <div className='flex-1 px-8 py-12'>
+        <div className='flex-1 px-8 py-12 grid'>
 
-          <main className='max-w-7xl mx-auto'>
+          <main className='max-w-7xl mx-auto w-full'>
             {children}
           </main>
         </div>
